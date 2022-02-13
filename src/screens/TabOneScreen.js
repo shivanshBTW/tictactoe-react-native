68f76694bab7e90221de8b93b75b8b2616733164
Shivansh Tyagi <stshivansh9@gmail.com>
2022-02-13 06:11:30 +0530

    wip ttt

--- src/screens/TabOneScreen.js
+++ src/screens/TabOneScreen.js
@@ -1,35 +1,47 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-
-export default function TabOneScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
-}
+import { StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
+
+function TabOneScreen({ navigation }) {
+  let columnCount = 3;
+  let rowCount = 3;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Quick TicTacToe</Text>
+      <View style={styles.gameContainer}>
+        {Array.from(new Array(rowCount)).map((_, index) => {
+          return (
+            <View style={styles.rowContainer}>
+              {Array.from(new Array(columnCount)).map((__, index2) => {
+                return (
+                  <View style={styles.cell}>
+                    <Text>GG</Text>
+                  </View>
+                );
+              })}
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+}
+
+export default TabOneScreen;
